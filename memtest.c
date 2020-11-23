0a952bd97092616a65d67187f6301c525e19517a
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-11-23 11:48:05 +0100

    initial commit

---
+++ memtest.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    char s;
+    printf("Do you want to allocate memory? y/n");
+    scanf("%s",s);
+    printf("You choosen: %s",s);
+
+    if ( s == "y" )
+    {
+      char* buffer = (char*) calloc(3000000000,1);
+      printf("Done allocating\n");
+    }
+    free(buffer);
+    return 0;
+}
---
+++ strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Harry Potter";
+
+  printf("\n");
+
+  printf("%c", *name);     // Output: H
+  printf("%c", *(name+1));   // Output: a
+  printf("%c", *(name+7));   // Output: o
+
+  printf("\n");
+
+  printf("%c", *name);     // Output: H
+  printf("%c", name[1]);   // Output: a
+  printf("%c", name[7]);   // Output: o
+
+  char *namePtr;
+
+  printf("\n");
+
+  namePtr = name;
+  printf("%c", *namePtr);     // Output: H
+  printf("%c", *(namePtr+1));   // Output: a
+  printf("%c", *(namePtr+7));   // Output: o
+}

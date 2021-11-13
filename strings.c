f1b1ffc81b620d1aa90b92f88d67b93d0ee9864c
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-11-13 19:50:37 +0100

    style: indent with tabs, not spaces;

--- strings.c
+++ strings.c
@@ -5,22 +5,22 @@ int main(void) {
 
   printf("\n");
 
-  printf("%c", *name);     // Output: H
-  printf("%c", *(name+1));   // Output: a
-  printf("%c", *(name+7));   // Output: o
+  printf("%c", *name);		// Output: H
+  printf("%c", *(name+1));	// Output: a
+  printf("%c", *(name+7));	// Output: o
 
   printf("\n");
 
-  printf("%c", *name);     // Output: H
-  printf("%c", name[1]);   // Output: a
-  printf("%c", name[7]);   // Output: o
+  printf("%c", *name);		// Output: H
+  printf("%c", name[1]);	// Output: a
+  printf("%c", name[7]);	// Output: o
 
   char *namePtr;
 
   printf("\n");
 
   namePtr = name;
-  printf("%c", *namePtr);     // Output: H
-  printf("%c", *(namePtr+1));   // Output: a
-  printf("%c", *(namePtr+7));   // Output: o
+  printf("%c", *namePtr);	// Output: H
+  printf("%c", *(namePtr+1));	// Output: a
+  printf("%c", *(namePtr+7));	// Output: o
 }
